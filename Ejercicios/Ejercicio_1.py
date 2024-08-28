9e1509e42ed5038a276e9b7295c16287a936995e
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-08-28 12:02:54 -0400

    Ejercicio 1

--- Ejercicios/Ejercicio_1.py
+++ Ejercicios/Ejercicio_1.py
@@ -1,20 +1,28 @@
-
 class Persona():
-    # Constructor de Clase
-    def __init__(self, nombre, apellido, edad):
+    def __init__(self, nombre, apellido, edad, altura, peso):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+        self.altura = float(altura)
+        self.peso = float(peso)
     
-        # Métodos
     def hablar(self):
-        print(f"{self.nombre} está hablando")
-
+        print(self.nombre, "está hablando")
+    
     def caminar(self):
         print(f"{self.nombre} está caminando")
+    
+    def estudiar(self):
+        print(f"{self.nombre} está estudiando")
+    
+    def saber_imc(self):
+        imc = self.peso / (self.altura * self.altura)
+        print("El imc de", self.nombre, "es:", imc)
 
-persona1 = Persona()
 
-persona1.nombre = "Cristina"
-persona1.apellido = "Torres"
-persona1.edad = 23
+persona1 = Persona("Cristina", "Torres", 20, 1.6, 65)
+
+print(persona1.caminar())
+print(persona1.hablar())
+print(persona1.estudiar())
+print(persona1.saber_imc())
---
+++ Ejercicios/Ejercicio_2.py
@@ -0,0 +1,6 @@
+class Coche():
+    def __init__(self, marca, gasolina):
+        self.marca = marca
+        self.gasolina = gasolina
+    # Metodos
+    def conducir()
